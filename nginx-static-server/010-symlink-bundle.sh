9f05a04e04e0d5cd51596e57c32003b57324f5a3
Alois <alois.barreras@getethos.com>
2020-07-20 11:15:39 -0400

    adding a startup check for env vars to nginx static server

--- nginx-static-server/010-symlink-bundle.sh
+++ nginx-static-server/010-symlink-bundle.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ -z $DEPLOY_ENVIRONMENT ]
+then
+  echo "DEPLOY_ENVIRONMENT is required to be set"
+  exit 1
+fi
+
 if [ $DEPLOY_ENVIRONMENT != "staging" ] && [ $DEPLOY_ENVIRONMENT != "production" ]
 then
   echo "Allowed values for DEPLOY_ENVIRONMENT are: staging, production"
