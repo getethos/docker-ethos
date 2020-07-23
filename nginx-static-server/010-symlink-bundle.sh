606cc0051fba0d3117552d2cafa9e9735f631fea
Alois <alois.barreras@getethos.com>
2020-07-23 15:55:00 -0400

    using NGINX_SERVE_STATIC_FILES_PATH env var to set the path where nginx serves static files

--- nginx-static-server/010-symlink-bundle.sh
+++ nginx-static-server/010-symlink-bundle.sh
@@ -2,16 +2,10 @@
 
 set -e
 
-if [ -z $DEPLOY_ENVIRONMENT ]
+if [ -z $NGINX_SERVE_STATIC_FILES_PATH ]
 then
   echo "DEPLOY_ENVIRONMENT is required to be set"
   exit 1
 fi
 
-if [ $DEPLOY_ENVIRONMENT != "staging" ] && [ $DEPLOY_ENVIRONMENT != "production" ]
-then
-  echo "Allowed values for DEPLOY_ENVIRONMENT are: staging, production"
-  exit 1
-fi
-
-ln -s /app/dist/$DEPLOY_ENVIRONMENT /usr/share/nginx/html
+ln -s $NGINX_SERVE_STATIC_FILES_PATH /usr/share/nginx/html
