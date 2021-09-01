993af6eb23d5e7bc1a88479c56ef680a16c7772e
Alois <alois.barreras@getethos.com>
2021-09-01 12:20:22 -0400

    adding test files

--- gatsby-static-server/test/test.sh
+++ gatsby-static-server/test/test.sh
@@ -1,77 +1,27 @@
 #! /usr/bin/env bash
 
-# adapted from https://serverfault.com/a/919042
-# returns the status code from curl'ing an endpoint
-get_status_code() {
-  local result=$(curl -s -o /dev/null -w "%{http_code}" $1)
-  echo $result
-}
+set -e
 
-get_redirect_url() {
-  local result=$(curl -s -o /dev/null -w "%{redirect_url}" $1)
-  echo $result
-}
+source ./util.sh
 
-wait_for_it() {
-  until curl --silent $1 > /dev/null; do
-    echo "host is unavailable - sleeping"
-    sleep 5
-  done
-}
+CONTAINER=$(start_nginx)
+trap "docker stop $CONTAINER" EXIT
 
-dcu() {
-  docker-compose -f docker-compose.test.yaml up --build -d
-}
-
-dcd() {
-  docker-compose -f docker-compose.test.yaml down
-}
-
-# Checks an expected status code against nginx response
-# Takes two parameters:
-# - (string) host:port of endpoint to hit
-# - (number) expected status code
-test_status_code() {
-  local host=$1
-  local expected=$2
-  echo "Testing $host for status code $expected..."
-  local result=$(get_status_code $host)
-  echo "Got $result response from $host"
-  if [[ $result != $expected ]]; then
-    echo "$result was not the expected response of $expected. Exiting."
-    exit 1
-  fi
-}
-
-# Checks an expected redirect URL code against nginx response
-# Takes two parameters:
-# - (string) host:port of endpoint to hit
-# - (number) expected redirect URL
-test_redirect_url() {
-  local host=$1
-  local expected=$2
-  echo "Testing $host for redirect URL $expected..."
-  local result=$(get_redirect_url $host)
-  echo "Got $result response from $host"
-  if [[ $result != $expected ]]; then
-    echo "$result was not the expected response of $expected. Exiting."
-    exit 1
-  fi
-}
-
-dcu
 echo "Waiting for nginx to become ready..."
 wait_for_it localhost:8080
 
-test_status_code localhost:8080 200
-test_status_code localhost:8080/ 200
-test_status_code localhost:8080/index.html 200
-test_status_code localhost:8080/agents 200
-test_status_code localhost:8080/agents/ 200
-test_status_code localhost:8080/foo 301
-test_redirect_url localhost:8080/foo http://localhost:8080/app
-test_status_code localhost:8080/term/preapply/quote 301
+test_status_code localhost:8080                      200
+test_status_code localhost:8080/                     200
+test_status_code localhost:8080/index.html           200
+test_status_code localhost:8080/agents               301
+test_status_code localhost:8080/agents/              200
+test_status_code localhost:8080/foo                  404
+test_status_code localhost:8080/term/preapply/quote  301
 test_status_code localhost:8080/term/preapply/quote/ 301
-test_status_code localhost:8080/unknown 200
+test_status_code localhost:8080/unknown              404
 
-dcd
+test_redirect_url localhost:8080/foo ""
+test_redirect_url localhost:8080/term/preapply/quote http://localhost:8080/app
+test_redirect_url "localhost:8080/term/preapply/quote?foo=bar&ethos=cool" "http://localhost:8080/app?foo=bar&ethos=cool"
+
+echo "All tests passed."
---
+++ gatsby-static-server/test/util.sh
@@ -0,0 +1,70 @@
+start_nginx() {
+  docker run -p 8080:8080 \
+    -e "NGINX_SERVE_STATIC_FILES_PATH=/app/dist/production" \
+    -e "NGINX_LISTEN_PORT=8080" \
+    -e "NGINX_METRICS_LISTEN_PORT=8081" \
+    -d \
+    -v `pwd`/fixtures/redirects.conf:/etc/nginx/conf.d/redirects/redirects.conf \
+    -v `pwd`/fixtures/public:/app/dist/production \
+    "$@" \
+    getethos/gatsby-static-server:test
+}
+
+# adapted from https://serverfault.com/a/919042
+# returns the status code from curl'ing an endpoint
+get_status_code() {
+  local result=$(curl -s -o /dev/null -w "%{http_code}" $1)
+  echo $result
+}
+
+get_redirect_url() {
+  local result=$(curl -s -o /dev/null -w "%{redirect_url}" $1)
+  echo $result
+}
+
+wait_for_it() {
+  until curl --silent $1 > /dev/null; do
+    echo "host is unavailable - sleeping"
+    sleep 5
+  done
+}
+
+dcu() {
+  docker-compose -f docker-compose.test.yaml up --build -d
+}
+
+dcd() {
+  docker-compose -f docker-compose.test.yaml down
+}
+
+# Checks an expected status code against nginx response
+# Takes two parameters:
+# - (string) host:port of endpoint to hit
+# - (number) expected status code
+test_status_code() {
+  local host=$1
+  local expected=$2
+  echo "Testing $host for status code $expected..."
+  local result=$(get_status_code $host)
+  echo "Got $result response from $host"
+  if [[ $result != $expected ]]; then
+    echo "$result was not the expected response of $expected. Exiting."
+    exit 1
+  fi
+}
+
+# Checks an expected redirect URL code against nginx response
+# Takes two parameters:
+# - (string) host:port of endpoint to hit
+# - (number) expected redirect URL
+test_redirect_url() {
+  local host=$1
+  local expected=$2
+  echo "Testing $host for redirect URL $expected..."
+  local result=$(get_redirect_url $host)
+  echo "Got $result response from $host"
+  if [[ $result != $expected ]]; then
+    echo "$result was not the expected response of $expected. Exiting."
+    exit 1
+  fi
+}
